6e2c767e4119e16fb39b6b47d667927bc818f2f3
Evgeny Ganusenko <evgan66@gmail.comevgan66@gmail.comevgan66@gmail.com>
2022-02-24 22:38:43 +0300

    add OperationQueue

--- GrandCentralDispatch/Sources/Conroller/ViewController.swift
+++ GrandCentralDispatch/Sources/Conroller/ViewController.swift
@@ -9,29 +9,105 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    private var onboardingView: SettingsView? {
+   // MARK: - Properties
+    var onboardingView: SettingsView? {
         guard isViewLoaded else { return nil }
         return view as? SettingsView
     }
 
-   private var isBlack: Bool = false {
+    // MARK: - Сomputed properties
+    private var isBlack: Bool = true {
         didSet {
             if isBlack {
-                self.view.backgroundColor = .black
+                changeObjectsColor(isBlack: isBlack)
             } else {
-                self.view.backgroundColor = .white
+                changeObjectsColor(isBlack: isBlack)
             }
         }
     }
 
+    // MARK: - viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         view = SettingsView()
+        configurate(status: .start)
+        onboardingView?.changeStatusButton.addTarget(self, action: #selector(tapButton), for: .touchUpInside)
+        onboardingView?.buttonColorReplacement.addTarget(self, action: #selector(tapButtonColorChange), for: .touchUpInside)
+        
     }
 }
 
 extension ViewController {
-    
+
+    // MARK: - Action object changes
+    @objc func tapButton() {
+        configurate(status: .process)
+        let password = onboardingView?.textField.text ?? ""
+        let queue = OperationQueue()
+        let passwordArray = password.components(withMaxLength: ViewControllerConstants.stringWithMaxLength)
+        var operation = [GenerateBruteForceOperation]()
+        for password in passwordArray {
+            operation.append(GenerateBruteForceOperation(password: password))
+        }
+        for force in operation {
+            queue.addOperation(force)
+        }
+
+        queue.addBarrierBlock { [weak self] in
+            DispatchQueue.main.async {
+                self?.configurate(status: .finished)
+            }
+        }
+    }
+
+    @objc func tapButtonColorChange() {
+        isBlack.toggle()
+    }
+
+    private func changeObjectsColor (isBlack: Bool) {
+        switch isBlack {
+        case false:
+            self.view.backgroundColor = .black
+            self.onboardingView?.infoLabel.textColor = .white
+            self.onboardingView?.buttonColorReplacement.backgroundColor = .systemPink
+        case true:
+            self.view.backgroundColor = .white
+            self.onboardingView?.infoLabel.textColor = .black
+            self.onboardingView?.buttonColorReplacement.backgroundColor = .systemOrange
+        }
+    }
+
+    private func configurate(status: Status) {
+        switch status {
+        case .start:
+            onboardingView?.infoLabel.text = "Произвести взлом?"
+            onboardingView?.changeStatusButton.setTitle("Взлом", for: .normal)
+        case .process:
+            onboardingView?.infoLabel.text = "Выполняется взлом. Это не займет много времени..."
+            onboardingView?.textField.text = String.generateRandom(long: ViewControllerConstants.textFieldMaxRandomElements)
+            onboardingView?.activityIndicator.isHidden = false
+            onboardingView?.activityIndicator.startAnimating()
+            onboardingView?.textField.isSecureTextEntry = true
+            onboardingView?.buttonColorReplacement.isHidden = false
+            onboardingView?.changeStatusButton.isUserInteractionEnabled = false
+            onboardingView?.changeStatusButton.setTitle("Взламываю...", for: .normal)
+        case .finished:
+            onboardingView?.activityIndicator.isHidden = true
+            onboardingView?.activityIndicator.stopAnimating()
+            onboardingView?.textField.isSecureTextEntry = false
+            onboardingView?.infoLabel.text = "Пароль \(self.onboardingView?.textField.text ?? "")"
+            onboardingView?.changeStatusButton.isSelected = false
+            onboardingView?.buttonColorReplacement.isHidden = true
+            onboardingView?.changeStatusButton.isUserInteractionEnabled = true
+            onboardingView?.changeStatusButton.setTitle("Повтор", for: .normal)
+        }
+    }
+}
+
+// MARK: - Cnstants
+struct ViewControllerConstants {
+    static let stringWithMaxLength: Int = 3
+    static let textFieldMaxRandomElements: Int = 10
 }
 
--- GrandCentralDispatch/Sources/GenerateBruteForceOperation.swift
+++ GrandCentralDispatch/Sources/GenerateBruteForceOperation.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
-class GenerateBruteForceOperation {
+class GenerateBruteForceOperation: Operation {
+
+    private var password: String
+
+    init(password: String) {
+        self.password = password
+    }
+
+    override func main() {
+        super.main()
+        if isCancelled {
+            return
+        }
+        bruteForce(passwordToUnlock: password)
+    }
 
     // Сравнивает заданный пароль со сгенерированной строкой. Если сгенерированная строка совпадает с заданным паролем, метод выводит сгенерированную строку в консоль.
     func bruteForce(passwordToUnlock: String) {
@@ -36,8 +50,7 @@ class GenerateBruteForceOperation {
         var result: String = string
         if result.count <= 0 {
             result.append(characterAt(index: 0, array))
-        }
-        else {
+        } else {
             result.replace(at: result.count - 1, with: characterAt(index: (indexOf(character: result.last!, array) + 1) % array.count, array))
             if indexOf(character: result.last!, array) == 0 {
                 result = String(generateBruteForce(String(result.dropLast()), fromArray: array)) + String(result.last!)
